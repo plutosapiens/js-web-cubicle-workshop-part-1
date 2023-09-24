1ea86786a170f5d2698cab79dd441724d6b477c1
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-09-24 21:03:25 +0300

    CUB-1 - Initialize handlebars, setup first default view

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
+
 const app = express();
 const PORT = 5555;
 
+//Handlebars config
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
 app.get("/", (req, res) => {
-    res.send("Hello from express server!");
+    // res.send("Hello from express server!");
+    res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
