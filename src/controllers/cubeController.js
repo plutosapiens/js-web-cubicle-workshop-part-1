522f0911515c4cb2799787c733da53fb61773b18
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-09-29 21:38:03 +0300

    CUB-1-Implement search functionality in the home page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -2,19 +2,30 @@ const router = require("express").Router();
 const cubeService = require("../services/cubeService")
 
 router.get("/create", (req, res) => {
-    console.log(cubeService.getAll());
     res.render("create");
 });
 
 router.post('/create', (req, res) => {
-    const { name, descriotion, imgUrl, difficultyLevel } = req.body
+    const { name, description, imgUrl, difficultyLevel } = req.body
     cubeService.create({
         name,
-        descriotion,
+        description,
         imgUrl,
         difficultyLevel: Number(difficultyLevel),
     });
     res.redirect('/')
 })
+
+router.get('/:cubeId/details', (req, res) => {
+    const { cubeId } = req.params;
+    const cube = cubeService.getSingleCube(cubeId);
+
+    if(!cube) {
+        res.redirect('/404');
+        return
+    }
+
+    res.render('details', { ...cube });
+});
 
 module.exports = router;
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -4,21 +4,21 @@ const cubes = [
     {
         id: '1kows4je8gln4ofpko',
         name: 'cubcho',
-        descriotion: 'bebe kub',
+        description: 'bebe kub',
         imgUrl: 'https://m.media-amazon.com/images/I/61fB-s4DPVS.jpg',
         difficultyLevel: 1
       },
       {
         id: '1kows4je8gln4og3nn',
         name: 'Ивет',
-        descriotion: 'shes a cube also',
+        description: 'shes a cube also',
         imgUrl: 'https://mathworld.wolfram.com/images/eps-svg/Cube_700.svg',
         difficultyLevel: 5
       },
       {
         id: '1kows4je8gln4ogn2i',
         name: 'zzz',
-        descriotion: 'mmmboy',
+        description: 'mmmboy',
         imgUrl: 'https://cdn1.byjus.com/wp-content/uploads/2021/03/Cube-net.png',
         difficultyLevel: 5
       }
@@ -34,6 +34,24 @@ exports.create = (cubeData) => {
     return newCube
 }
 
-exports.getAll = () => {
-    return [...cubes];
+exports.getAll = (search, from, to) => {
+  let filterCubes = [...cubes];
+  if(search) {
+    filterCubes = filterCubes.filter((cube) => cube.name.toLowerCase().includes(search));
+  }
+  if(from) {
+    filterCubes = filterCubes.filter(
+      (cube) => cube.difficultyLevel >= Number(from)
+    );
+  }
+  if(to) {
+    filterCubes = filterCubes.filter(
+      (cube) => cube.difficultyLevel <= Number(to)
+    );
+  }
+    return filterCubes;
+};
+
+exports.getSingleCube = (id) => {
+  return cubes.find((cube) => cube.id ===id);
 };
